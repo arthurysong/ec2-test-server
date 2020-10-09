b5144a14a74d19c8e995dce485408c4ea7023d8e
Arthur <arthursong14@gmail.com>
2020-10-09 11:00:21 -0700

    Server

---
+++ index.js
@@ -0,0 +1,26 @@
+    require('dotenv').config();
+    
+    const express = require('express');
+    const cors = require('cors');
+    const http = require('http');
+    
+    const PORT = process.env.PORT || 5000;
+    
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    
+    const server = http.createServer(app);
+    
+    // Router for server
+    // app.use(router);
+
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: "ALLO WORLD" })
+    })
+    
+    // Server listening on the port given
+    server.listen(PORT, () => {
+        console.log(`Server has started on port ${PORT}`);
+    });
+    
