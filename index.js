c08501dd30b833884d9f6fc0de2317238e99d50b
Arthur <arthursong14@gmail.com>
2020-10-09 11:27:32 -0700

    Port 5000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
     const cors = require('cors');
     const http = require('http');
     
-    const PORT = process.env.PORT || 80;
+    const PORT = process.env.PORT || 5000;
     
     const app = express();
     app.use(cors());
